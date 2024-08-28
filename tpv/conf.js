15bc855bd8b172fead47864d15a8a52bc511e76a
beja benabid <snopidz@gmail.com>
2024-08-28 20:01:48 +0200

    Create conf.js

---
+++ tpv/conf.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+  strings: ['السلام عليكم👋', 'Bonjour 👋', 'Hello 👋', '你好 🙌', 'Привет 🙏', 'こんにちは 🙏', 'வணக்கம் 🖐'],
+  autoStart: true,
+  loop: true,
+});
+
