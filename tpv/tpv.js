4a35663c9841c159bd20ce3ff8980a30ede9d267
beja benabid <snopidz@gmail.com>
2024-08-28 18:52:21 +0200

    Create tpv.js

---
+++ tpv/tpv.js
@@ -0,0 +1,36 @@
+const typewriterText = document.querySelector(".typewriter");
+const phrases = [
+  "Salut ",
+  "Hello 👋",
+  "Bonjour 👋",
+  "你好 🙌",
+  "Привет 🙏",
+  "こんにちは 🙏",
+  "வணக்கம் 🖐"
+];
+
+let phraseIndex = 0;
+let characterIndex = 0;
+
+function type() {
+  if (characterIndex < phrases[phraseIndex].length) {
+    typewriterText.textContent += phrases[phraseIndex][characterIndex];
+    characterIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1000);
+  }
+}
+
+function erase() {
+  if (characterIndex > 0) {
+    typewriterText.textContent = phrases[phraseIndex].substring(0, characterIndex - 1);
+    characterIndex--;
+    setTimeout(erase, 50);
+  } else {
+    phraseIndex = (phraseIndex + 1) % phrases.length;
+    setTimeout(type, 250);
+  }
+}
+
+type();
